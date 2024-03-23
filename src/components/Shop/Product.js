6a1ffab8379fa8facec365da151af463d5292a61
mbagher26 <mohammadbaghermollanory@gmail.com>
2024-03-23 06:11:10 -0400

    change class to className and put dinamic value

--- src/components/Shop/Product.js
+++ src/components/Shop/Product.js
@@ -1,16 +1,22 @@
 import React, { Component } from 'react'
+import './styles/Product.css'
+
 
 export default class Product extends Component {
     
+    
     render() {
+
+        let { title, price, img} = this.props
+
         return (
-            <div class="shop-item">
-                <span class="shop-item-title">Shirt</span>
-                <img class="shop-item-image" alt='' src='Images/Shirt.png' />
-                <div class="shop-item-details">
-                    <span class="shop-item-price">$129</span>
+            <div className="shop-item">
+                <span className="shop-item-title">{this.props.title}</span>
+                <img className="shop-item-image" alt={title} src={img} />
+                <div className="shop-item-details">
+                    <span className="shop-item-price">{price}</span>
                     <button
-                        class="btn btn-primary shop-item-button"
+                        className="btn btn-primary shop-item-button"
                         type="button">
                         ADD TO CART
                     </button>
