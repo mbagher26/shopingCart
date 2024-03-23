c815b111aa629bab930c0dcc7a1224bed18d9ffc
mbagher26 <mohammadbaghermollanory@gmail.com>
2024-03-23 05:35:01 -0400

    chsnge class to className

--- src/components/Shop/CartProduct.js
+++ src/components/Shop/CartProduct.js
@@ -5,15 +5,15 @@ export default class CartProduct extends Component {
     render() {
 
         return (
-            <div class="cart-row">
-                <div class="cart-item cart-column">
-                    <img class="cart-item-image" alt='' src="Images/Shirt.png" width="100" height="100" />
-                    <span class="cart-item-title">Shirt</span>
+            <div className="cart-row">
+                <div className="cart-item cart-column">
+                    <img className="cart-item-image" alt='' src="./Images/Cofee.png" width="100" height="100" />
+                    <span className="cart-item-title">Shirt</span>
                 </div>
-                <span class="cart-price cart-column">$198</span>
-                <div class="cart-quantity cart-column">
+                <span className="cart-price cart-column">$198</span>
+                <div className="cart-quantity cart-column">
 
-                    <button class="btn btn-danger" type="button">REMOVE</button>
+                    <button className="btn btn-danger" type="button">REMOVE</button>
                 </div>
             </div>
         )
