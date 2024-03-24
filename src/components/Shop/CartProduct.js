021d1fdf6a29675b2b2b1639fd770d8b532fa945
mbagher26 <mohammadbaghermollanory@gmail.com>
2024-03-24 05:53:26 -0400

    put dinamic value for tags

--- src/components/Shop/CartProduct.js
+++ src/components/Shop/CartProduct.js
@@ -7,10 +7,10 @@ export default class CartProduct extends Component {
         return (
             <div className="cart-row">
                 <div className="cart-item cart-column">
-                    <img className="cart-item-image" alt='' src="./Images/Cofee.png" width="100" height="100" />
-                    <span className="cart-item-title">Shirt</span>
+                    <img className="cart-item-image" alt='' src={this.props.img} width="100" height="100" />
+                    <span className="cart-item-title">{this.props.title}</span>
                 </div>
-                <span className="cart-price cart-column">$198</span>
+                <span className="cart-price cart-column">{this.props.price}</span>
                 <div className="cart-quantity cart-column">
 
                     <button className="btn btn-danger" type="button">REMOVE</button>
