12b3fcc82291b4c1a3392c8e31682cac78a4f85a
mbagher26 <mohammadbaghermollanory@gmail.com>
2024-03-27 09:29:46 -0400

    get and placemant props

--- src/components/Shop/Social.js
+++ src/components/Shop/Social.js
@@ -2,11 +2,11 @@ import React, { Component } from 'react'
 
 export default class Social extends Component {
     render() {
-        console.log(this.props, 'props');
+        let { href, img} = this.props
         return (
             <li>
-                <a href="sabzlearn.ir" target="_blank">
-                    <img alt='' src="Images/YouTube Logo.png" />
+                <a href={href}>
+                    <img alt={href} src={img} style={{width: '90px' , height: '90px'}}/>
                 </a>
             </li>
         )
