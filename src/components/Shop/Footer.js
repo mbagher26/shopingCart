6357abf9d40d8c1e65a5ebc179ba1cae2a06d62f
mbagher26 <mohammadbaghermollanory@gmail.com>
2024-03-27 12:57:10 -0400

    import Footer.css and update className with band-title

--- src/components/Shop/Footer.js
+++ src/components/Shop/Footer.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import Social from './Social'
+import './styles/Footer.css'
 
 export default class Footer extends Component {
     constructor(props) {
@@ -19,7 +20,7 @@ export default class Footer extends Component {
             <>
                 <footer className="main-footer">
                     <div className="container main-footer-container">
-                        <h3 className="band-name">The Generics</h3>
+                        <h3 className="band-title">The Generics</h3>
                         <ul className="nav footer-nav">
                             {
                                 this.state.socials && this.state.socials.map((social) =>(
