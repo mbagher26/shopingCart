0e6dc0c76d3986746abc57fc1dcec53d38e22226
mbagher26 <mohammadbaghermollanory@gmail.com>
2024-04-11 09:54:07 -0400

    refactor to function component

--- src/components/Shop/Social.js
+++ src/components/Shop/Social.js
@@ -1,14 +1,32 @@
-import React, { Component } from 'react'
+// import React, { Component } from 'react'
 
-export default class Social extends Component {
-    render() {
-        let { href, img} = this.props
-        return (
-            <li>
-                <a href={href}>
-                    <img alt={href} src={img}/>
-                </a>
-            </li>
-        )
-    }
+// export default class Social extends Component {
+//     render() {
+//         let { href, img} = this.props
+//         return (
+//             <li>
+//                 <a href={href}>
+//                     <img alt={href} src={img}/>
+//                 </a>
+//             </li>
+//         )
+//     }
+// }
+
+// ///////////////////
+// refactor to function component
+
+import React from 'react'
+
+function Social(props) {
+    let {href, img} = props
+    return (
+        <li>
+            <a href={href}>
+                <img alt={href} src={img} />
+            </a>
+        </li>
+    )
 }
+
+export default Social
