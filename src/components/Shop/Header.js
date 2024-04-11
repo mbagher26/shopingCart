b2a9a7c7fb6d9bc511a0b88297cf9bc6a0da3c56
mbagher26 <mohammadbaghermollanory@gmail.com>
2024-04-11 09:50:52 -0400

    refactor to function component

--- src/components/Shop/Header.js
+++ src/components/Shop/Header.js
@@ -1,29 +1,52 @@
-import React, { Component } from 'react'
+// import React, { Component } from 'react'
 import './styles/Header.css'
 
-export default class Header extends Component {
-    constructor(props) {
-        super(props)
+// export default class Header extends Component {
+//     constructor(props) {
+//         super(props)
 
-        this.state = {
+//         this.state = {
 
-        }
-    }
+//         }
+//     }
 
-    render() {
-        return (
-            <>
-                <header className="main-header">
-                    <nav className="main-nav nav">
-                        <ul>
-                            <li><a href="javascript">HOME</a></li>
-                            <li><a href="/#">STORE</a></li>
-                            <li><a href="/#">ABOUT</a></li>
-                        </ul>
-                    </nav>
-                    <h1 className="band-name band-name-large">Superstore</h1>
-                </header>
-            </>
-        )
-    }
+//     render() {
+//         return (
+//             <>
+//                 <header className="main-header">
+//                     <nav className="main-nav nav">
+//                         <ul>
+//                             <li><a href="javascript">HOME</a></li>
+//                             <li><a href="/#">STORE</a></li>
+//                             <li><a href="/#">ABOUT</a></li>
+//                         </ul>
+//                     </nav>
+//                     <h1 className="band-name band-name-large">Superstore</h1>
+//                 </header>
+//             </>
+//         )
+//     }
+// }
+// ////////////////////
+// refactor to function component
+import React from 'react'
+
+function Header() {
+    return (
+        <>
+            <header className="main-header">
+                <nav className="main-nav nav">
+                    <ul>
+                        <li><a href="javascript">HOME</a></li>
+                        <li><a href="/#">STORE</a></li>
+                        <li><a href="/#">ABOUT</a></li>
+                    </ul>
+                </nav>
+                <h1 className="band-name band-name-large">Superstore</h1>
+            </header>
+        </>
+    )
 }
+
+export default Header
+
